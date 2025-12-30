b894568dad427fffeb2040fa3c08e50492869806
Infectslide <infectslide@gmail.com>
2025-12-30 08:54:25 +0000

    Added options but they only work for the files after them in the CLA

--- src/toywc.c
+++ src/toywc.c
@@ -3,12 +3,14 @@
 #include <unistd.h>
 #include <fcntl.h>
 #define BUFFSIZE 100
-
+int l ,w ,c;
+int tl, tw, tc;
 int updcount(char* , int* , int* , int*, char* , int);
+void printwc(int , int , int);
+void printtwc(int, int, int);
 char buff[BUFFSIZE];
 
 int main(int argc, char *argv[argc+1]){
-    int l,w,c, tl, tw, tc;
     int wf,cf,lf;
     FILE* fp;
     int bread = 0;
@@ -18,7 +20,8 @@ int main(int argc, char *argv[argc+1]){
         while((bread = read(STDIN_FILENO, buff, BUFFSIZE)) > 0){
             updcount(buff, &l, &w, &c, &wcond, bread);
         }
-        printf("%7d %7d %7d\n", l , w , c);
+        printwc(lf, wf, cf);
+        printf("\n");
     }
     else {
         for (char** clarg = argv + 1;--argc > 0; clarg++){
@@ -28,10 +31,11 @@ int main(int argc, char *argv[argc+1]){
                     while((bread = read(STDIN_FILENO, buff, BUFFSIZE)) > 0){
                         updcount(buff, &l, &w, &c, &wcond, bread);
                     }
-                    printf("%d %d %d -\n", l , w , c);
+                    printwc(lf, wf, cf);
+                    printf(" -\n");
                 }
                 else {
-                    for(char* clopt = *clarg; *clopt != '\0'; clopt++){
+                    for(char* clopt = *clarg + 1; *clopt != '\0'; clopt++){
                         switch(*clopt){
                         case 'l':
                             lf = 1;
@@ -55,7 +59,8 @@ int main(int argc, char *argv[argc+1]){
                     while((bread = read(fp->_fileno, buff, BUFFSIZE)) > 0){
                         updcount(buff, &l, &w, &c, &wcond, bread);
                     }
-                    printf("%d %d %d %s\n", l , w , c, *clarg);
+                    printwc(lf, wf, cf);
+                    printf(" %s\n", *clarg);
                 }
                 else {
                     fprintf(stderr, "%s: %s: No such file or directory\n",argv[0], *clarg);
@@ -65,7 +70,8 @@ int main(int argc, char *argv[argc+1]){
             tw += w;
             tc += c;
         }
-        printf("%d %d %d total\n", tl , tw , tc);
+        printtwc(lf,wf,cf);
+        printf(" total\n");
     }
 }
 
@@ -92,4 +98,52 @@ int updcount(char* s, int* l, int* w, int* c,char* wcond, int bread){
     *w += nw;
     *c += nc;
     return 0;
+}
+
+void printwc(int fl, int fw, int fc){
+    if((fl && fw && fc) || !(fl || fw || fc)){
+        printf("%7d %7d %7d", l, w ,c);
+    }
+    else if(fl && fw){
+        printf("%7d %7d", l , w);
+    }
+    else if(fl && fc){
+        printf("%7d %7d", l, c);
+    }
+    else if(fw && fc){
+        printf("%7d %7d", w, c);
+    }
+    else if(fw){
+        printf("%7d", w);
+    }
+    else if(fc){
+        printf("%7d", c);
+    }
+    else if(fl){
+        printf("%7d", l);
+    }
+}
+
+void printtwc(int fl, int fw, int fc){
+    if((fl && fw && fc) || !(fl || fw || fc)){
+        printf("%7d %7d %7d", tl, tw ,tc);
+    }
+    else if(fl && fw){
+        printf("%7d %7d", tl , tw);
+    }
+    else if(fl && fc){
+        printf("%7d %7d", tl, tc);
+    }
+    else if(fw && fc){
+        printf("%7d %7d", tw, tc);
+    }
+    else if(fw){
+        printf("%7d", tw);
+    }
+    else if(fc){
+        printf("%7d", tc);
+    }
+    else if(fl){
+        printf("%7d", tl);
+    }
 }
